1f8d9f2fd69febd80deded8335ba678e1fd674b0
Chinmay Pillai <chinmay.pillai@gmail.com>
2023-07-03 22:26:33 +0530

    User Authentication

--- Server/index.js
+++ Server/index.js
@@ -2,14 +2,38 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const User = require('./models/UserModel');
 
 app.use(cors());
 app.use(express.json());
-mongoose.connect('mongodb://localhost:27017');
+mongoose.connect('mongodb://127.0.0.1:27017/mern-ecommerse-website').then(() => console.log('Connected to Mongoose'));
 
 
-app.get('/', (req, res) => {
-    res.send('Welcome');
+app.post('/login', async (req, res) => {
+    const user = await User.findOne({
+        email: req.body.email,
+        password: req.body.password
+    })
+    if(user)
+        res.send({ status: "OK", user: user});
+    else
+        res.send({ status: "Error", message: "Can't Find User, Please check input or Register" });
+})
+
+app.post('/register', async (req, res) => {
+    console.log(req.body)
+    try{
+        await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        })
+        res.send(req.body);
+    }
+    catch(err){
+        console.log(err)
+        res.send(err.message);
+    }
 })
 
 app.listen(3000, () => console.log('Server Started on port 3000'));
