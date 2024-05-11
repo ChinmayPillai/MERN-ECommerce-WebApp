6c80d5f0caf63f2b5b88cd44aaa3eea649dcc2d1
Chinmay Pillai <chinmay.pillai@gmail.com>
2024-05-12 03:25:47 +0530

    Integrate Redis in Cart

--- Server/routes/cart.js
+++ Server/routes/cart.js
@@ -5,13 +5,18 @@ const redis = require('../redis-client');
 
 cartRouter.get('/:id', async (req, res) => {
     try{
-        // const user = await redis.call('JSON.GET', 'user:'+req.params.id)
+        let user = await redis.call('JSON.GET', 'user:'+req.params.id)
 
-        // if(!user){
-            const user = await User.findById(req.params.id)
-            // redis.call('JSON.SET', 'user:'+req.params.id, '.', JSON.stringify(user))
-            // redis.expire("user:"+req.params.id, 3600)
-        // }
+        if(!user){
+            console.log('Setting user in Redis');
+            user = await User.findById(req.params.id)
+            redis.call('JSON.SET', 'user:'+req.params.id, '.', JSON.stringify(user))
+            redis.expire("user:"+req.params.id, 3600)
+        }
+        else{
+            user = JSON.parse(user);
+        }
+        console.log(user);
         res.send(user.cart);
     }
     catch(err){
@@ -28,6 +33,7 @@ cartRouter.post('/:id', async (req, res) => {
         //console.log(user);
         await user.cart.unshift(req.body.item);
         await user.save();
+        await redis.call('JSON.SET', 'user:'+req.params.id, '.cart', JSON.stringify(user.cart));
         res.send('Added to Cart');
         return;
         }
@@ -42,17 +48,20 @@ cartRouter.put('/:id', async (req, res) => {
         //console.log(req.params.id + '\n' + req.body.item.id + '\n')
         if(req.body.action === 'increment'){
             console.log(`Incrementing Quantity of ${req.params.id}\n`);
-            const user = await User.findOneAndUpdate({'_id': req.params.id, 'cart.id': req.body.item.id}, {'$inc': {'cart.$.quantity': 1}})
+            const user = await User.findOneAndUpdate({'_id': req.params.id, 'cart.id': req.body.item.id}, {'$inc': {'cart.$.quantity': 1}}, { new: true })
+            await redis.call('JSON.SET', 'user:'+req.params.id, '.cart', JSON.stringify(user.cart));
             res.send(user);
         }
         else if(req.body.action === 'decrement'){
             console.log(`Decrementing Quantity of ${req.params.id}\n`);
-            const user = await User.findOneAndUpdate({'_id': req.params.id, 'cart.id': req.body.item.id}, {'$inc': {'cart.$.quantity': -1}})
+            const user = await User.findOneAndUpdate({'_id': req.params.id, 'cart.id': req.body.item.id}, {'$inc': {'cart.$.quantity': -1}}, { new: true })
+            await redis.call('JSON.SET', 'user:'+req.params.id, '.cart', JSON.stringify(user.cart));
             res.send(user);
         }
         else if(req.body.action === 'delete'){
             console.log(`Deleting ${req.params.id}\n`);
-            const user = await User.findOneAndUpdate({'_id': req.params.id}, {'$pull': {'cart': {id: req.body.item.id}}})
+            const user = await User.findOneAndUpdate({'_id': req.params.id}, {'$pull': {'cart': {id: req.body.item.id}}}, { new: true })
+            await redis.call('JSON.SET', 'user:'+req.params.id, '.cart', JSON.stringify(user.cart));
             res.send(user);
         }else if(req.body.action === 'clear'){
             const user = await User.findByIdAndUpdate(
@@ -60,6 +69,7 @@ cartRouter.put('/:id', async (req, res) => {
                 { $set: { cart: [] } },
                 { new: true }
             );
+            await redis.call('JSON.SET', 'user:'+req.params.id, '.cart', JSON.stringify(user.cart));
             console.log(user.cart)
             res.send(user);
         }
