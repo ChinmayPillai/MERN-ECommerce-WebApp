aa62b4ddb0100a6ee8b40396814a38e6287aa0cd
Chinmay Pillai <chinmay.pillai@gmail.com>
2024-05-12 03:25:11 +0530

    Integrate Redis in Wishlist

--- Server/routes/wishlist.js
+++ Server/routes/wishlist.js
@@ -5,13 +5,17 @@ const redis = require('../redis-client');
 
 wishlistRouter.get('/:id', async (req, res) => {
     try{
-        // const user = await redis.call('JSON.GET', 'user:'+req.params.id)
+        let user = await redis.call('JSON.GET', 'user:'+req.params.id)
 
-        // if(!user){
-            const user = await User.findById(req.params.id)
-        //     redis.call('JSON.SET', 'user:'+req.params.id, '.', JSON.stringify(user))
-        //     redis.expire("user:"+req.params.id, 3600)
-        // }
+        if(!user){
+            user = await User.findById(req.params.id)
+            redis.call('JSON.SET', 'user:'+req.params.id, '.', JSON.stringify(user))
+            redis.expire("user:"+req.params.id, 3600)
+        }
+        else{
+            user = JSON.parse(user);
+        }
+        console.log(user);
         res.send(user.wishlist);
     }
     catch(err){
@@ -27,6 +31,7 @@ wishlistRouter.post('/:id', async (req, res) => {
         
         await user.wishlist.unshift(req.body.item);
         await user.save();
+        await redis.call('JSON.SET', 'user:'+req.params.id, '.wishlist', JSON.stringify(user.wishlist));
         res.send('Added to Wishlist');
         return;
         }
@@ -40,7 +45,8 @@ wishlistRouter.put('/:id', async (req, res) => {
     try{
         console.log(req.params.id + '\n' + req.body.item.id + '\n')
         if(req.body.action === 'delete'){
-            const user = await User.findOneAndUpdate({'_id': req.params.id}, {'$pull': {'wishlist': {id: req.body.item.id}}})
+            const user = await User.findOneAndUpdate({'_id': req.params.id}, {'$pull': {'wishlist': {id: req.body.item.id}}}, { new: true })
+            await redis.call('JSON.SET', 'user:'+req.params.id, '.wishlist', JSON.stringify(user.wishlist));
             res.send(user);
         }
     }
